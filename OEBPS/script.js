344e172cbcee25a41d4a6bf3b44a114886307d9c
micschk <mvanschaik@gmail.com>
2012-06-25 22:31:47 +0200

    Fixed some indentations & minor formatting

--- OEBPS/script.js
+++ OEBPS/script.js
@@ -10,9 +10,9 @@ $('.slidescontainer').each(function(){
 
 $(".slidecontr").click(function(event){
     event.stopPropagation();
-    if( $(this).hasClass('next') ){ i = 1; } 
-    else if( $(this).hasClass('prev') ) { i = -1; }
-    else { return false; }
+    if( $(this).hasClass('next') ){i = 1;} 
+    else if( $(this).hasClass('prev') ) {i = -1;}
+    else {return false;}
     curURL = $("img.slides",$(this).parent().parent()).attr('src');
     curimg = curURL.substr(curURL.length - 6, 2);
     //alert('curURL.substr(curURL.length - 6, 2): '+curURL.substr(curURL.length - 6, 2));
@@ -20,12 +20,12 @@ $(".slidecontr").click(function(event){
     //alert('nextimg = parseInt(curimg, 10)+i: '+(parseInt(curimg, 10)+i));
     slideSum = $("img.slides",$(this).parent().parent()).attr('rel');
     $('.prev,.next',$(this).parent().parent()).removeClass('inactive');
-    if( nextimg>=slideSum ){ $('.next',$(this).parent().parent()).addClass('inactive'); }
-    if( nextimg<=1 ){ $('.prev',$(this).parent().parent()).addClass('inactive'); }
-    if( nextimg<=0 ){ return false; }
-    if( nextimg>slideSum ){ return false; }
+    if( nextimg>=slideSum ){$('.next',$(this).parent().parent()).addClass('inactive');}
+    if( nextimg<=1 ){$('.prev',$(this).parent().parent()).addClass('inactive');}
+    if( nextimg<=0 ){return false;}
+    if( nextimg>slideSum ){return false;}
     $('.slideinfo',$(this).parent().parent()).text(nextimg+'/'+slideSum);
-    if( nextimg<10 ) { nextimg = '0'+nextimg; }
+    if( nextimg<10 ) {nextimg = '0'+nextimg;}
     nextURL = curURL.replace(curimg+'.jpg', nextimg+'.jpg');
     //alert('nextURL = curURL.replace(curimg, nextimg): '+nextURL = curURL.replace(curimg, nextimg));
     $("img.slides",$(this).parent().parent()).attr('src', nextURL);
@@ -35,46 +35,44 @@ $(".slidecontr").click(function(event){
 
 $('video').each(function(a){
 
-    var vid = $(this)[0];
-        vid.addEventListener('loadedmetadata', function() {
-            vid.currentTime = $(this).parent().attr('rel');
-      }, false);
+	var vid = $(this)[0];
+	vid.addEventListener('loadedmetadata', function() {
+		vid.currentTime = $(this).parent().attr('rel');
+	}, false);
 
-      vid.addEventListener("timeupdate", function() {
-        if (vid.currentTime >= $(this).parent().attr('alt')) {
-          vid.currentTime = $(this).parent().attr('rel');
-          $('.play').css('background-image','url(images/play.svg)'); 
-          vid.pause();
-        }}, false); 
+	vid.addEventListener("timeupdate", function() {
+		if (vid.currentTime >= $(this).parent().attr('alt')) {
+			vid.currentTime = $(this).parent().attr('rel');
+			$('.play').css('background-image','url(images/play.svg)'); 
+			vid.pause();
+		}
+	}, false); 
       
-      video1Play=false;
+	video1Play=false;
 
 });
 
 $('.reload').click(function() {
-    vidcont = $(this).parent().parent();
-    vid = $('video', vidcont)[0];
-     vid.currentTime = $('video', vidcont).parent().attr('rel');
-      return false;
-      });
+	vidcont = $(this).parent().parent();
+	vid = $('video', vidcont)[0];
+	vid.currentTime = $('video', vidcont).parent().attr('rel');
+	return false;
+});
 
 $('.play').click(function() {
-    vidcont = $(this).parent().parent();
-    vid = $('video', vidcont)[0];
-    if(video1Play == false){
-      vid.play();
-      video1Play=true;  
-      $(this).css('background-image','url(images/pause.svg)');        
-     }
-  else{
-   vid.pause();
-    video1Play=false;
-    $(this).css('background-image','url(images/play.svg)');  
-    }
-      })
-
-
-
+	vidcont = $(this).parent().parent();
+	vid = $('video', vidcont)[0];
+	if(video1Play == false){
+		vid.play();
+		video1Play=true;  
+		$(this).css('background-image','url(images/pause.svg)');        
+	}
+	else{
+		vid.pause();
+		video1Play=false;
+		$(this).css('background-image','url(images/play.svg)');  
+	}
+})
 
 
 });
